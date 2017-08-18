a556fd9810907fc79bfc8aa06431eed27294619d
Collin <Collinhassing@gmail.com>
2017-08-17 21:05:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,16 +18,21 @@ function pageScroll(){
 
 // scroll to top speed.
 function toTop(){
-scrollTo(document.body, 0, 800);
+if(navigator.userAgent.toLowerCase().indexOf('firefox') !== -1){
+        //firefox
+        scrollTo(document.documentElement, 0, 800);
+    }else{
+        //rest of navigators
+        scrollTo(document.body, 0, 800);
+    }
  }
 
 function scrollTo(element, to, duration){
     if(duration <= 0) return;
     var difference = to - element.scrollTop;
-    var time = difference / duration * 10;
-
+    var perTick = difference / duration * 10;
     setTimeout(function(){
-        element.scrollTop = element.scrollTop + time;
+        element.scrollTop = element.scrollTop + perTick;
         if(element.scrollTop == to) return;
         scrollTo(element, to, duration - 10);
     }, 10);
